97e726f55a2a4eeacf9f72a9f90840c9b977af3d
AmenallahMeftehh <meftah.amenallah@gmail.com>
2016-04-28 16:00:35 +0100

    ajout de book avec post recuperation des livres et/ou un seul livre seulement backend

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+// declaration d'express
 var express = require('express');
+// declaration de mongoose pour se connecter a mongodb
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/bookAPI')
+// declaration de bodyParser pour lire body et le parser en jason object
+var  bodyParser = require('body-parser');
+// connexion a mongodb avec la Base de données bookAPI
+var db = mongoose.connect('mongodb://localhost/bookAPIUP');
+// Appel au model book
+var Book = require ('./models/bookModel');
+// instantiation d'express
 var app = express();
+// declaration de port
 var port = process.env.PORT || 3000;
+// routage avec express
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-var bookRouter = express.Router();
-bookRouter.route('/Books')
-  .get(function(req,res){
-    var responseJson = {hello:'thism my api'};
-    res.json(responseJson);
-  })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
 app.use('/api',bookRouter);
-
-
 app.get('/',function(req,res){
   res.send('welcome to my API');
 });
